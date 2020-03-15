2b8d8965ed77a8991d3d3d41b9e544eb35b95419
byfebeef <jimmykang89@gmail.com>
2020-03-15 16:55:10 -0700

    heroku prep2

--- app.js
+++ app.js
@@ -13,6 +13,14 @@ app.use(passport.initialize());
 require('./config/passport')(passport);
 
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+    app.get('/', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    })
+}
+
+
 mongoose
     .connect(db, { useNewUrlParser: true })
     .then(() => console.log("connected to mongoDB~~") )
@@ -41,10 +49,3 @@ app.use('/api/tweets', tweets);
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {console.log(`Listening on port of ${port}`)});
-
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('frontend/build'));
-    app.get('/', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-    })
-}
